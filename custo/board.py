b51fc220fb04a722c7982ec53c168a490e52857e
bananalytics <31481575+bananalytics@users.noreply.github.com>
2018-08-29 11:00:51 +0200

    Define Board class, the basic unit of computation for custo
    Board will serve as the basic unit of computation for future
    Cutting Stock Problem algorithm implementations. Boards are cut into specific
    sizes, optimized to reduce waste.
    
    Board is initialized with material_size;
    Board carries a contents, made of pieces which can be added or removed;
    Board keeps a running total of the space left on it with space_remaining.
    
    Future functionality would include BoardCollection, a grouping of Boards.

--- custo/board.py
+++ custo/board.py
@@ -8,25 +8,31 @@ class Board(object):
     def insert(self, piece_length):
 
         if self.space_remaining >= piece_length:
-
             self.contents.append(piece_length)
             self.space_remaining -= piece_length
-
         else:
-            raise Exception('piece of length too long to be inserted')
+            raise ValueError('piece of length too long to be inserted')
 
     def remove(self, piece_length):
 
         if piece_length in self.contents:
-
             self.contents.remove(piece_length)
             self.space_remaining += piece_length
-
         else:
-            raise Exception('piece not on the Board!')
+            raise ValueError('piece not on the Board!')
 
     def __repr__(self):
-        return str( 'Board with items {}, unused space: {}'.format(self.contents, self.space_remaining))
+        return str('Board with items {}, unused space: {}'.format(self.contents, self.space_remaining))
 
     def __str__(self):
-        return self.contents
+        return str ( self.contents )
+
+if __name__ == '__main__':
+    a = Board(100)
+    print(a)
+    a.insert(50)
+    print(a)
+    a.insert(50)
+    print(a)
+    a.remove(50)
+    print(a)
---
+++ tests/test_board.py
@@ -0,0 +1,71 @@
+import unittest
+from unittest import TestCase
+
+from board import Board
+
+
+class TestBoard(TestCase):
+
+    # test "insert" Class method
+    def test_insert(self):
+
+        # Test for proper type o insert
+        a = Board(2050.)
+
+        self.assertRaises(Exception, a.insert, 'car')  # assert string fails
+
+        self.assertRaises(Exception, a.insert, [0, 1, 2])  # assert [] fails
+
+        try:
+            a.insert(100)    # assert int OK
+        except TypeError:
+            self.fail("insert() raised TypeError unexpectedly!")
+
+        try:
+            a.insert(100.0)    # assert float OK
+        except TypeError:
+            self.fail("insert() raised TypeError unexpectedly!")
+
+        # Test that you can't insert something big. (max board size = 2050)
+        b = Board(2050.)
+
+        with self.assertRaises(Exception):
+            b.insert(1000)
+            b.insert(1000)
+            b.insert(51)
+
+        b2 = Board(2050.)
+
+        self.assertRaises(Exception, b2.insert, 2051)
+
+        # Test space remaining works as planned
+        c = Board(2050.)
+
+        c.insert(100)
+
+        self.assertEqual(c.space_remaining, 1950)
+
+        c.insert(850)
+
+        self.assertEqual(c.space_remaining, 1100)
+
+    # test "remove" Class method
+    def test_remove(self):
+
+        b = Board(2050.)
+        b.insert(50)
+
+        # assert can't remove something that doesn't exist
+        self.assertRaises(Exception, b.remove, 100)
+        # assert space remaining works correctly
+        self.assertEqual(b.space_remaining, 2000)
+
+        b.remove(50)
+
+        # assert can't remove something that was recently removed
+        self.assertRaises(Exception, b.remove, 50)
+        # assert space remaining works correctly
+        self.assertEqual(b.space_remaining, 2050)
+
+if __name__ == '__main__':
+    unittest.main()
